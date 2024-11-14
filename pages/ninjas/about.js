c5008d104a331e0c1b32158827f3e689338fcce7
Alex <114432977+PasatAlexandru20@users.noreply.github.com>
2024-11-14 15:12:53 +0200

    Update about.js

--- pages/ninjas/about.js
+++ pages/ninjas/about.js
@@ -9,8 +9,8 @@ const About = () => {
       </Head>
         <div>
             <h1>About Page</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>At Ninjas, we’re not just tech enthusiasts; we’re versatile professionals with deep-rooted knowledge in IT systems, DevOps, software development, and business analytics. Our team is composed of experts who have honed their craft through real-world experience, certifications, and a commitment to staying at the forefront of tech innovation.</p>
+            <p>From sysadmin services, Docker containerization, and Ansible automation to front-end development and cloud architecture, we bring a full suite of skills that cater to the unique needs of each project. Our experience spans customer support, system administration, network engineering, and beyond, ensuring comprehensive and well-rounded support..</p>
         </div>
         </>
     )
